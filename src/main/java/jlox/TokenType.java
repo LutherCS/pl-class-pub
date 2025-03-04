d58038919c5b63ee7a750f8d34441c36177d258a
Roman Yasinovskyy <yasinovskyy@users.noreply.github.com>
2025-03-04 12:21:18 -0600

    Add Lox starting code

---
+++ src/main/java/jlox/TokenType.java
@@ -0,0 +1,27 @@
+package jlox;
+
+/**
+ * @author: Robert Nystrom
+ * @see: https://craftinginterpreters.com/
+ */
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
